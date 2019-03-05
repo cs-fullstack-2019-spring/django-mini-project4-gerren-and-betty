5faacd8b28421698cfdeafdc5806584fdc9e38c7
cierravereen24 <cierravereen24@gmail.com>
2019-03-05 11:26:35 -0600

    cc update

--- gameproject/gameapp/views.py
+++ gameproject/gameapp/views.py
@@ -1,8 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import GameModel
 
 
 # Create your views here.
 
 def index(request):
     return HttpResponse('test')
+
+
+@login_required
+def mygames(request):
+    list_of_games = GameModel.objects.filter(name=request.user)
+    context = {'list_of_games': list_of_games}
+    return render(request, 'gameapp/mygames.html', context)
