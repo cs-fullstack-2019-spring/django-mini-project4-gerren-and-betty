65453564cbbf8ce59329b7d096a8b200928271d6
cierravereen24 <cierravereen24@gmail.com>
2019-03-05 10:35:37 -0600

    c update

--- gameproject/gameapp/admin.py
+++ gameproject/gameapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CollectorModel
+from .models import GameModel
+
+
+
+admin.site.register(CollectorModel)
+admin.site.register(GameModel)
+
+
+
--- gameproject/gameapp/models.py
+++ gameproject/gameapp/models.py
@@ -6,9 +6,11 @@ from django.utils import timezone
 # Create your models here.
 
 
-class Collector(models.Model):
+class CollectorModel(models.Model):
     username = models.CharField(max_length=50)
     password1 = models.CharField(max_length=16)
     password2 = models.CharField(max_length=16)
     dateAccountCreated = models.DateField(timezone.now)
     userTableForeignKey = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+
